53ab07c40e56f9c1486b9f112334799fd126f719
James Halliday <mail@substack.net>
2012-08-10 06:04:56 -0700

    remove inactive tests

--- example/array.js
+++ example/array.js
@@ -1,6 +1,6 @@
 var split = require('../');
 var fs = require('fs');
 
-var src = fs.readFileSync(__dirname + '/../test/mixed.css', 'utf8');
+var src = fs.readFileSync(__dirname + '/style.css', 'utf8');
 var chunks = split(src);
 console.dir(chunks);
